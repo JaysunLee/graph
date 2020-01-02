3330d8fc841c356cf62c16fd2d0dc2288101f963
Jaysun Lee <lee.jaysun@gmail.com>
2020-01-02 20:00:34 +1100

    Implement mutations against a graph

--- ListGraph.cs
+++ ListGraph.cs
@@ -2,8 +2,59 @@ using System.Collections.Generic;
 
 namespace Graph
 {
+    // Undirected, unweighted graph
     class ListGraph
     {
         private Dictionary<Vertex, List<Vertex>> AdjacencyList { get; set; }
+
+        public ListGraph()
+        {
+            AdjacencyList = new Dictionary<Vertex, List<Vertex>>();
+        }
+
+        public void AddVertex(Vertex vertex)
+        {
+            if (!AdjacencyList.ContainsKey(vertex))
+            {
+                AdjacencyList.Add(vertex, new List<Vertex>());
+            }
+        }
+
+        public void RemoveVertex(Vertex vertex)
+        {
+            foreach (var adjacency in AdjacencyList.Values)
+            {
+                adjacency.Remove(vertex);
+            }
+            AdjacencyList.Remove(vertex);
+        }
+
+        public void AddEdge(Vertex vertexOne, Vertex vertexTwo)
+        {
+            var vertexOneAdjacents = AdjacencyList[vertexOne];
+            vertexOneAdjacents.Add(vertexTwo);
+            var vertexTwoAdjacents = AdjacencyList[vertexTwo];
+            vertexTwoAdjacents.Add(vertexOne);
+        }
+
+        public void RemoveEdge(Vertex vertexOne, Vertex vertexTwo)
+        {
+            var vertexOneAdjacents = AdjacencyList[vertexOne];
+            var vertexTwoAdjacents = AdjacencyList[vertexTwo];
+
+            if (vertexOneAdjacents != null)
+            {
+                vertexOneAdjacents.Remove(vertexTwo);
+            }
+            if (vertexTwoAdjacents != null)
+            {
+                vertexOneAdjacents.Remove(vertexOne);
+            }
+        }
+
+        public List<Vertex> GetAdjacentVertices(Vertex vertex)
+        {
+            return AdjacencyList[vertex];
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace Graph
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var graph = new ListGraph();
+            var bob = new Vertex("Bob");
+            var alice = new Vertex("Alice");
+            var mark = new Vertex("Mark");
+            var rob = new Vertex("Rob");
+            var maria = new Vertex("Maria");
+            graph.AddVertex(bob);
+            graph.AddVertex(alice);
+            graph.AddVertex(mark);
+            graph.AddVertex(rob);
+            graph.AddVertex(maria);
+            graph.AddEdge(bob, alice);
+            graph.AddEdge(bob, rob);
+            graph.AddEdge(alice, mark);
+            graph.AddEdge(rob, mark);
+            graph.AddEdge(alice, maria);
+            graph.AddEdge(rob, maria);
+
+            graph.RemoveEdge(alice, maria);
+            graph.RemoveVertex(mark);
+            foreach (var vertex in graph.GetAdjacentVertices(alice))
+            {
+                Console.WriteLine(vertex.Label);
+            }
         }
     }
 }
